2c52ae0bcfb020ac03d2ce6feaa6f3d74b820315
ido gueron <debigueron@Debis-MacBook-Air.local>
2020-06-23 11:42:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';
-import { observer } from 'mobx-react'
+import React from 'react';
+import { observer, inject } from 'mobx-react'
 
 
-@observer
-class App extends Component {
-
-  render() {
-  return <div>hello world</div>
-  }
-  
-}
+const App = observer((props) => {
+    
+    return (
+       <div>
+         hello world
+       </div>
+    )
+})
         
-
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react'
 
-ReactDOM.render(<App />,document.getElementById('root')
+// import {Store as store} from './stores/storeName'
+
+// const store = new Store()
+
+// const stores = {store}
+
+ReactDOM.render(<Provider {...stores}><App /></Provider>,document.getElementById('root')
 );
 
-
 serviceWorker.unregister();
+
